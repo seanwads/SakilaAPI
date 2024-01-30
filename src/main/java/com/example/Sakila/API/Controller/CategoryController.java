3648f2f15a82f58f5d73e7a31ed6f51dfabd8009
Sean Wadsworth <sw695@sussex.ac.uk>
2024-01-30 09:43:11 +0000

    fixed compilation error

--- src/main/java/com/example/Sakila/API/Controller/CategoryController.java
+++ src/main/java/com/example/Sakila/API/Controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.example.Sakila.API.Model.Category;
 import com.example.Sakila.API.Repository.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
 
 @CrossOrigin
 @RequestMapping("/cat")
@@ -14,6 +15,6 @@ public class CategoryController {
 
     @GetMapping("/get/{id}")
     private Category getCatById(@PathVariable("id") int catId){
-        return catRepository.findById(catId).orElse
+        return catRepository.findById(catId).orElseThrow(() -> new ResourceAccessException("Category not found"));
     }
 }

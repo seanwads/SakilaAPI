ef6e8218dacb804074e72531fd55f46b6720bd2f
Sean Wadsworth <sw695@sussex.ac.uk>
2024-02-29 10:47:35 +0000

    Added endpoint to add film to user inventory

--- src/main/java/com/example/Sakila/API/Controller/UserController.java
+++ src/main/java/com/example/Sakila/API/Controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.Sakila.API.Controller;
 
 import com.example.Sakila.API.Model.Film;
+import com.example.Sakila.API.Model.Inventory;
 import com.example.Sakila.API.Model.Rental;
+import com.example.Sakila.API.Repository.FilmRepository;
 import com.example.Sakila.API.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,18 +11,22 @@ import com.example.Sakila.API.Model.User;
 import org.springframework.web.client.ResourceAccessException;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @CrossOrigin
 @RequestMapping("/user")
 @RestController
 public class UserController {
     private final UserRepository userRepository;
+    private final FilmRepository filmRepository;
     private static final String notFoundResponse = "User not found";
 
     @Autowired
-    public UserController(UserRepository userRepository){
+    public UserController(UserRepository userRepository, FilmRepository filmRepository){
         this.userRepository = userRepository;
+        this.filmRepository = filmRepository;
     }
 
     @GetMapping("/get/{id}")
@@ -57,6 +63,31 @@ public class UserController {
         user.setRentalSet(updatedUser.getRentalSet());
         user.setEmailAddress(updatedUser.getEmailAddress());
         user.setActive(updatedUser.getActive());
+
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/addFilm/{userId}/{filmId}")
+    public User addFilmToUser(@PathVariable("userId") int userId, @PathVariable("filmId") int filmId){
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceAccessException(notFoundResponse));
+        Film film = filmRepository.findById(filmId).orElseThrow(() -> new ResourceAccessException("Film not found"));
+
+        for(Rental rental : user.getRentalSet()){
+            if(rental.getRentalInventory().getInventoryFilm().getFilmId() == filmId){
+                return user;
+            }
+        }
+
+        Inventory inventory = new Inventory();
+        Rental rental = new Rental();
+
+        inventory.setInventoryFilm(film);
+        rental.setRentalInventory(inventory);
+        rental.setRentalCustomer(user);
+
+        Set<Rental> userFilms = new HashSet<>(user.getRentalSet());
+        userFilms.add(rental);
+        user.setRentalSet(userFilms);
 
         return userRepository.save(user);
     }
